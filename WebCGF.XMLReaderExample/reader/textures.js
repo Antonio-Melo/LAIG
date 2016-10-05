fd901b0e5532d9f7a6e2f68335b20a8740d8f615
Ariana <up201404789@fe.up.pt>
2016-10-05 23:17:22 +0100

    Textures
    textures.js created

---
+++ WebCGF.XMLReaderExample/reader/textures.js
@@ -0,0 +1,20 @@
+function Textures(node,reader){
+
+    this.reader = new CGFXMLreader();
+
+    this.node = node;
+
+
+    this.id = this.reader.getString(node,'id');
+    this.file = this.reader.getString(node,'file');
+    this.length_s = this.reader.getFloat(node,'length_s');
+    this.length_t = this.reader.getFloat(node,'length_t');
+
+
+
+  console.debug(this.id + '\n');
+  console.debug(this.file + '\n');
+	console.debug(this.length_s + '\n');
+  console.debug(this.length_t + '\n');
+
+};
