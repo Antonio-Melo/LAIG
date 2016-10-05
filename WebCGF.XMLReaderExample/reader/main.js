20027f66e4bb957185695372b27cbeca9d2fc628
Ariana <up201404789@fe.up.pt>
2016-10-05 23:20:44 +0100

    Main
    added new files

--- WebCGF.XMLReaderExample/reader/main.js
+++ WebCGF.XMLReaderExample/reader/main.js
@@ -11,7 +11,9 @@ function getUrlVars() {
     return vars;
 }
 
-serialInclude(['../lib/CGF.js', 'XMLscene.js', 'MySceneGraph.js','primitives/Rectangle.js','primitives/Triangle.js','primitives/Cylinder.js','primitives/Sphere.js','primitives/Torus.js',
+serialInclude(['../lib/CGF.js', 'XMLscene.js', 'MySceneGraph.js','primitives/Rectangle.js',
+'primitives/Triangle.js','primitives/Cylinder.js','primitives/Sphere.js','views.js', 'illumination.js',
+'omni.js', 'spot.js', 'textures.js', 'materials.js', 'transformations.js',
 
 main=function()
 {
