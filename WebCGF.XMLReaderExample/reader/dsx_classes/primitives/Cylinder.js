681df106daca4fd6787e0b179e9a49cbdce88d5a
Ariana <up201404789@fe.up.pt>
2016-10-10 13:07:24 +0100

    Display Cylinder and Sphere v1
    Cylinder without cover
    Just half sphere

--- WebCGF.XMLReaderExample/reader/dsx_classes/primitives/Cylinder.js
+++ WebCGF.XMLReaderExample/reader/dsx_classes/primitives/Cylinder.js
@@ -2,7 +2,9 @@
   Class that represents a cylinder primitive in the scene
 */
 
-function Cylinder(node){
+
+ function Cylinder(node,scene) {
+ 	CGFobject.call(this,scene);
 
   this.node = node;
 
@@ -13,4 +15,54 @@ function Cylinder(node){
   this.slices = node.attributes.getNamedItem("slices").value;
   this.stacks = node.attributes.getNamedItem("stacks").value;
   console.debug("Cylinder "+this.base+"\n");
+ 	this.initBuffers();
+ };
+
+ Cylinder.prototype = Object.create(CGFobject.prototype);
+ Cylinder.prototype.constructor = Cylinder;
+
+ Cylinder.prototype.initBuffers = function() {
+
+
+	var counter = 0;
+	var n = 0;
+	var angle = 2 * Math.PI / this.slices;
+	this.vertices = [];
+	this.indices = [];
+	this.normals = [];
+	this.texCoords = [];
+
+
+for(j = 0; j < this.stacks; j++){
+	for( i = 0; i < this.slices; i++ ){
+
+		this.vertices.push(Math.cos(counter), Math.sin(counter), (j+1)*(1/this.stacks));
+		this.vertices.push(Math.cos(counter), Math.sin(counter),  (j)*(1/this.stacks));
+		this.normals.push(Math.cos(counter), Math.sin(counter), 0);
+		this.normals.push(Math.cos(counter), Math.sin(counter), 0);
+		this.texCoords.push(0.5+0.5*Math.cos(counter), j+1);
+		this.texCoords.push(0.5+0.5*Math.cos(counter), j);
+
+		counter += angle;
+		this.vertices.push(Math.cos(counter), Math.sin(counter), (j+1)*(1/this.stacks));
+		this.vertices.push(Math.cos(counter), Math.sin(counter),  (j)*(1/this.stacks));
+		this.normals.push(Math.cos(counter), Math.sin(counter), 0);
+		this.normals.push(Math.cos(counter), Math.sin(counter), 0);
+		this.texCoords.push(0.5+0.5*Math.cos(counter), j+1);
+		this.texCoords.push(0.5+0.5*Math.cos(counter), j);
+
+		this.indices.push(n++);
+		this.indices.push(n++);
+		this.indices.push(n++);
+		this.indices.push(n--);
+		this.indices.push(n--);
+		this.indices.push(n++);
+
+		n += 2;
+
+	}
 }
+
+ 	this.primitiveType = this.scene.gl.TRIANGLES;
+ 	this.initGLBuffers();
+ };
