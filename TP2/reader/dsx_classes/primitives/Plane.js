c1155b5a36e7ad769d7d60ef94e7b157979f6bc5
Antonio-Melo <happyd3nt@gmail.com>
2016-11-21 01:06:26 +0000

    Plane done

--- TP2/reader/dsx_classes/primitives/Plane.js
+++ TP2/reader/dsx_classes/primitives/Plane.js
@@ -3,7 +3,6 @@
  * @constructor
  */
 function Plane(scene,id,dimX,dimY,partsX,partsY){
-
   this.scene = scene;
   this.id = id;
   this.dimX = dimX;
@@ -11,8 +10,45 @@ function Plane(scene,id,dimX,dimY,partsX,partsY){
   this.partsX = partsX;
   this.partsY = partsY;
 
-  CGFnurbsObject.call(this,scene);
+  var knots1 = this.getKnotsVector(1);
+  var knots2 = this.getKnotsVector(1);
+  var controlPoints = [];
+
+  var halfDimX = this.dimX/2;
+  var haltDimY = this.dimY/2;
+
+  var temp = [
+    [-halfDimX,-haltDimY,0,1],
+    [-halfDimX,haltDimY,0,1]
+  ];
+  var temp1 = [
+    [halfDimX,-haltDimY,0,1],
+    [halfDimX,haltDimY,0,1]
+  ];
+  controlPoints.push(temp);
+  controlPoints.push(temp1);
+
+  var surface = new CGFnurbsSurface(1,1,knots1,knots2,controlPoints);
+  getSurfacePoint = function(u,v){
+    return surface.getPoint(u,v);
+  };
+
+  CGFnurbsObject.call(this,scene,getSurfacePoint,this.partsX,this.partsY);
+
 };
 
 Plane.prototype = Object.create(CGFnurbsObject.prototype);
 Plane.prototype.constructor = Plane;
+
+Plane.prototype.getKnotsVector = function (degree) {
+ var vector = [];
+
+ for(var i = 0; i <= degree;i++){
+   vector.push(0);
+ }
+ for(var i = 0; i <= degree;i++){
+   vector.push(1);
+ }
+
+ return vector;
+};
