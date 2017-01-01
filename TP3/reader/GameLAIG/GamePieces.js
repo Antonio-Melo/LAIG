daa43e38541adc4e1822883747933b094e5072da
Antonio-Melo <happyd3nt@gmail.com>
2017-01-01 20:37:46 +0000

    All pieces but with wrong id

--- TP3/reader/GameLAIG/GamePieces.js
+++ TP3/reader/GameLAIG/GamePieces.js
@@ -9,7 +9,7 @@
 
   for(var i = 0; i < 5;i++){
     for(var f = 1;f < 6;f++){
-      if(listofPieces[i*5+f-1] == 'x'){
+      if(listofPieces[i*5+f-1] == 'x' || listofPieces[i*5+f-1] == 'center'){
       }else{
         var type = listofPieces[i*5+f-1][0].charAt(0);
         var player = listofPieces[i*5+f-1][0].charAt(1);
@@ -17,7 +17,7 @@
         console.debug(ii.toString()+f.toString());
         var house = houses.list[ii.toString()+f.toString()];
         console.debug(house);
-        this.list[listofPieces[i*5+f-1][0]] = new Piece(this.scene,listofPieces[i*5+f-1][0],player,type,house);
+        this.list[ii.toString()+f.toString()] = new Piece(this.scene,listofPieces[i*5+f-1][0],player,type,house);
       }
     }
   }
